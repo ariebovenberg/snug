dcfaf9a505d2d71f52910340ea949554589f53a4
Arie Bovenberg <ariebovenberg@wlnss.com>
2017-06-14 18:46:27 +0200

    implement bind resources to session class

--- omgorm.py
+++ omgorm.py
@@ -2,6 +2,18 @@
 import collections
 
 
+class Session(object):
+    """the context in which resources are used"""
+
+    def __init_subclass__(cls, **kwargs):
+        cls.resources = {}
+        super().__init_subclass__(**kwargs)
+
+    @classmethod
+    def register_resource(cls, resource_cls: type) -> None:
+        cls.resources[resource_cls.__name__] = resource_cls
+
+
 class Field(object):
     """an attribute accessor for a resource"""
 
@@ -9,11 +21,21 @@ class Field(object):
         self.resource, self.name = resource, name
 
 
-class Resource(object):
+class Resource:
     """base class for API resources"""
 
-    def __init_subclass__(cls, **kwargs):
-        cls._fields = collections.OrderedDict(
+    def __init_subclass__(cls, session_cls: type, **kwargs):
+        """initialize a Resource subclass
+
+        Parameters
+        ----------
+        session_cls
+            the :class:`Session` subclass to bind to this resource
+        """
+        session_cls.register_resource(cls)
+        cls.session_cls = session_cls
+        cls.fields = collections.OrderedDict(
             (name, obj) for name, obj in cls.__dict__.items()
             if isinstance(obj, Field)
         )
+        super().__init_subclass__(**kwargs)
