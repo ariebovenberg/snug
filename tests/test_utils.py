9ad217ff1f50245f04949adb01201d04b5748776
Arie Bovenberg <ariebovenberg@wlnss.com>
2017-06-18 20:04:43 +0200

    add test for ppartial bug

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,7 +12,7 @@ class TestPPartial:
     def test_is_subclass(self):
         assert issubclass(utils.ppartial, functools.partial)
 
-    def test_positional_args(self):
+    def test_placeholder(self):
         func = utils.ppartial(_example_func, ..., ..., 'foo', ...,
                               bla='bing', another='thing')
 
@@ -20,3 +20,10 @@ class TestPPartial:
 
         assert args == (1, 2, 'foo', 3)
         assert kwargs == {'bla': 'bing', 'another': 'thing2'}
+
+    def test_no_placeholders(self):
+        func = utils.ppartial(_example_func, 'foo', 5)
+        args, kwargs = func(10)
+
+        assert args == ('foo', 5, 10)
+        assert not kwargs
