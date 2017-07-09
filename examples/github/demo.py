0821e57501547b12770860ac7c9a2d52b0c2d12c
Arie Bovenberg <ariebovenberg@wlnss.com>
2017-07-09 09:12:04 +0200

    update github example

--- examples/github/demo.py
+++ examples/github/demo.py
@@ -1,7 +1,12 @@
+import requests
 import github
 
 
-session = github.Session()
+req_session = requests.Session()
+req_session.headers.update(github.HEADERS)
+
+
+session = github.Session(req_session)
 
 org = session.Organization.get('github')
 print('retrieving organization: {}'.format(org))
--- examples/github/github.py
+++ examples/github/github.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 import urllib
-from typing import Tuple
 
 import snug
 
@@ -8,34 +7,45 @@ partial = snug.utils.ppartial
 get_full_url = partial(urllib.parse.urljoin, 'https://api.github.com/')
 parse_datetime = partial(datetime.strptime, ..., '%Y-%m-%dT%H:%M:%SZ')
 
+HEADERS = {'Accept': 'application/vnd.github.v3+json'}
+
 
 class Session(snug.Session):
-    """github session with simple credentials"""
+    pass
 
-    def __init__(self, auth: Tuple[str, str]=None):
-        super().__init__()
-        if auth:
-            self.requests.auth = auth
-        self.requests.headers.update({
-            # explicit API header recommended by docs
-            'Accept': 'application/vnd.github.v3+json'
-        })
+
+class Repo(snug.json.Resource, session_cls=Session):
+    name = snug.Field()
+
+
+def load_repos(url):
+    import pdb; pdb.set_trace()
 
 
 class Organization(snug.json.Resource, session_cls=Session):
-    name = snug.Field()
+    avatar_url = snug.Field()
+    blog = snug.Field()
+    company = snug.Field()
     created_at = snug.Field(load=parse_datetime)
     description = snug.Field()
     email = snug.Field()
+    events_url = snug.Field()
     followers = snug.Field()
     following = snug.Field()
     has_organization_projects = snug.Field()
     has_repository_projects = snug.Field()
+    hooks_url = snug.Field()
+    html_url = snug.Field()
     id = snug.Field()
+    issues_url = snug.Field()
     location = snug.Field()
     login = snug.Field()
+    members_url = snug.Field()
+    name = snug.Field()
     public_gists = snug.Field()
+    public_members_url = snug.Field()
     public_repos = snug.Field()
+    repos_url = snug.Field(load=load_repos)
     type = snug.Field()
 
     @classmethod
