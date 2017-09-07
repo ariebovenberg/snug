a0a22952aec16a2a552a4afa343743885c55d130
Arie Bovenberg <ariebovenberg@wlnss.com>
2017-09-07 08:25:20 +0200

    implement Slots base class

--- snug/utils.py
+++ snug/utils.py
@@ -1,5 +1,7 @@
 """Miscellaneous tools and boilerplate"""
-from functools import reduce, partial as _partial
+import abc
+
+from functools import reduce, partial as _partial, partialmethod
 
 
 def identity(obj):
@@ -17,8 +19,8 @@ class partial(_partial):
     by use of ellipsis (...).
     Useful for builtin python functions which do not take keyword args
 
-        >>> count_down_from = partial(range, ..., 0, -1)
-        >>> list(count_down_from(3))
+        >>> countdown = partial(range, ..., 0, -1)
+        >>> list(countdown(3))
         [3, 2, 1]
     '''
     def __call__(self, *args, **keywords):
@@ -27,3 +29,67 @@ class partial(_partial):
                        for a in self.args)
         merged_keywords = {**self.keywords, **keywords}
         return self.func(*merged_args, *iter_args, **merged_keywords)
+
+
+class SlotsMeta(abc.ABCMeta):
+
+    def __new__(self, name, bases, ns):
+
+        fields = ns.pop('__annotations__', {})
+
+        defaults = {}
+        for field in fields:
+            try:
+                defaults[field] = ns.pop(field)
+            except KeyError:
+                pass
+
+        ns.update({
+            '__slots__': fields,
+            'DEFAULTS': defaults,
+        })
+        return super().__new__(self, name, bases, ns)
+
+
+class Slots(metaclass=SlotsMeta):
+    """base class for simple data containers.
+    Basically namedtuple without tuple."""
+
+    def __init__(self, *args, **kwargs):
+        fields = self.__slots__
+        pos_args = dict(zip(fields, args))
+        duplicates = pos_args.keys() & kwargs.keys()
+        if duplicates:
+            raise TypeError(f'duplicate argument(s): {tuple(duplicates)}')
+
+        all_args = {**self.DEFAULTS, **pos_args, **kwargs}
+        missing = set(fields) - all_args.keys()
+        if missing:
+            raise TypeError(f'missing argument(s): {tuple(missing)}')
+
+        for name, value in all_args.items():
+            setattr(self, name, value)
+
+    def __eq__(self, other):
+        fields = self.__slots__
+        return (
+            isinstance(other, self.__class__)
+            and all(
+                v1 == v2
+                for v1, v2 in zip(
+                    map(self.__getattribute__, fields),
+                    map(other.__getattribute__, fields)
+                )
+            )
+        )
+
+    def __repr__(self):
+        kls = self.__class__
+        fields_repr = ', '.join([
+            f'{name}={getattr(self, name)!r}'
+            for name in self.__slots__
+        ])
+        return f'{kls.__module__}.{kls.__name__}({fields_repr})'
+
+    def astuple(self):
+        return tuple(map(self.__getattribute__, self.__slots__))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,8 @@
 import operator
 import functools
+import typing as t
+
+import pytest
 
 from snug import utils
 
@@ -43,3 +46,142 @@ class TestCompose:
             utils.partial(operator.truediv, 1)
         )
         assert func(4) == '5.25'
+
+
+class TestSlots:
+
+    def test_slots(self):
+
+        class Post(utils.Slots):
+            title:     str
+            author_id: int
+            body:      str = ''
+            archived:  bool = False
+
+        assert isinstance(Post, type)
+        assert Post.__bases__ == (utils.Slots, )
+        assert Post.__slots__ == {'title': str,
+                                  'author_id': int,
+                                  'body': str,
+                                  'archived': bool}
+
+        post = Post('hello', 4, 'my message', True)
+
+        assert isinstance(post, Post)
+        assert post.title == 'hello'
+        assert post.author_id == 4
+        assert post.body == 'my message'
+        assert post.archived is True
+
+        # can't set invalid attributes
+        with pytest.raises(AttributeError, match='date'):
+            post.date = 'foo'
+
+    def test_eq(self):
+
+        class Post(utils.Slots):
+            title:     str
+            author_id: int
+            archived:  bool
+
+        post1 = Post('hello', 6, False)
+        post2 = Post('hello', 0, False)
+        post3 = Post('hello', 6, False)
+
+        assert post1 == post3
+        assert not post1 != post3
+        assert post1 != post2
+        assert not post1 == post2
+
+    def test_init(self):
+
+        class Post(utils.Slots):
+            title:     str
+            author_id: int
+            body:      str = '<no message>'
+            archived:  bool = False
+
+        post = Post('hello', author_id=5, archived=True)
+        assert post == Post('hello', 5, '<no message>', True)
+
+        # duplicate arg
+        with pytest.raises(TypeError, match='author_id'):
+            post = Post('hi', 9, author_id=4, archived=False)
+
+        # missing arg
+        with pytest.raises(TypeError, match='author_id'):
+            post = Post('hello', archived=False)
+
+    def test_repr(self):
+
+        class Post(utils.Slots):
+            title:     str
+            author_id: int
+            archived:  bool
+
+        post = Post('hello world', author_id=4, archived=False)
+
+        assert repr(post) == (f'{Post.__module__}.Post(title=\'hello world\', '
+                              'author_id=4, archived=False)')
+
+    def test_empty(self):
+
+        class User(utils.Slots):
+            pass
+
+        assert not User.__slots__
+
+    def test_mixes_with_abstract_base_classes(self):
+
+        from collections.abc import Sequence, Callable, Collection
+
+        class Email(Callable, Sequence, utils.Slots):
+            sender:     str
+            recipients: t.List[str]
+            body:       str = ''
+
+            def __getitem__(self, index):
+                return self.recipients[index]
+
+            def __len__(self):
+                return len(self.recipients)
+
+            def __call__(self):
+                return 'sent email!'
+
+        email = Email('my.email@test.com', recipients=[
+            'bob@acme.com', 'wilma@company.org'
+        ])
+
+        assert isinstance(email, Collection)
+        assert isinstance(email, Sequence)
+        assert isinstance(email, Callable)
+        assert len(email) == 2
+        assert email() == 'sent email!'
+
+    def test_override_method(self):
+
+        class Post(utils.Slots):
+            title:     str
+            author_id: int
+            archived:  bool = False
+
+            def __init__(self, title, *args, **kwargs):
+                super().__init__(title.upper(), *args, **kwargs)
+
+        post = Post('Hello', author_id=5)
+
+        assert post.title == 'HELLO'
+        assert post.author_id == 5
+
+    def test_tuple(self):
+
+        class Post(utils.Slots):
+            title:     str
+            author_id: int
+            body:      str = '<no message>'
+            archived:  bool = False
+
+        post = Post('hi', author_id=9)
+
+        assert post.astuple() == ('hi', 9, '<no message>', False)
