24a705a60f0e3a5f18d42b70651432c1f0ae7bb3
Arie Bovenberg <ariebovenberg@wlnss.com>
2018-01-06 20:56:18 +0100

    refactor out query module

--- snug/core.py
+++ snug/core.py
@@ -1,10 +1,12 @@
 """the central abstractions"""
 import abc
 import typing as t
-from functools import partial
+from dataclasses import make_dataclass
+from functools import partial, partialmethod
 from types import GeneratorType
 
-from .utils import compose, nest, dclass, yieldmap, sendmap, returnmap
+from .utils import (compose, nest, dclass, yieldmap, sendmap, returnmap,
+                    func_to_fields, apply, as_tuple)
 
 __all__ = [
     'Query',
@@ -15,6 +17,7 @@ __all__ = [
     'yieldmapped',
     'sendmapped',
     'returnmapped',
+    'query',
 ]
 
 
@@ -121,3 +124,36 @@ class returnmapped:
 
     def __call__(self, func):
         return compose(partial(returnmap, self.func), func)
+
+
+class query:
+    """Create a query class from a generator function
+
+    Example
+    -------
+
+    >>> @query.cls_from_gen()
+    ... def post(id: int):
+    ...     return json.loads((yield f'posts/{id}/'))
+
+    Note
+    ----
+    The function must:
+
+    * be a python function, bound to a module.
+    * be fully annotated, without keyword-only arguments
+    """
+    def __call__(self,
+                 func: t.Callable[..., t.Generator[T_req, T_resp, T]]) -> (
+                     t.Type[Query[T, T_req, T_resp]]):
+        return make_dataclass(
+            func.__name__,
+            func_to_fields(func),
+            bases=(Query, ),
+            namespace={
+                '__doc__':     func.__doc__,
+                '__module__':  func.__module__,
+                '__iter__': partialmethod(compose(
+                    partial(apply, func), as_tuple)),
+            }
+        )
--- tests/test_core.py
+++ tests/test_core.py
@@ -71,3 +71,51 @@ def test_returnmapped():
     gen = decorated(5)
     assert next(gen) == 5
     assert genresult(gen, 103) == '103'
+
+
+def test_query_class():
+
+    class post(snug.Query):
+        def __init__(self, id):
+            self.id = id
+
+        def __iter__(self):
+            return (yield f'/posts/{self.id}/').decode('ascii')
+
+    query = post(id=2)
+    gen = iter(query)
+    assert isinstance(query, snug.Query)
+    assert next(gen) == '/posts/2/'
+    assert genresult(gen, b'hello') == 'hello'
+
+
+def test_generator_is_query():
+
+    def mygen():
+        yield
+        return
+
+    gen = mygen()
+    assert isinstance(gen, snug.Query)
+
+
+def test_query_decorator():
+
+    @snug.query()
+    def post(id: int):
+        """my docstring..."""
+        return (yield f'/posts/{id}/').decode('ascii')
+
+    assert issubclass(post, snug.Query)
+    assert post.__name__ == 'post'
+    assert post.__doc__ == 'my docstring...'
+    assert post.__module__ == 'test_core'
+    assert len(post.__dataclass_fields__) == 1
+
+    post34 = post(34)
+    assert isinstance(post34, snug.Query)
+    assert post34.id == 34
+
+    resolver = iter(post34)
+    assert next(resolver) == '/posts/34/'
+    assert genresult(resolver, b'hello') == 'hello'
