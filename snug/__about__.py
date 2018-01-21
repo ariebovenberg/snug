3e039fc81fa8a04e8c0f02699e299b118df6acec
Arie Bovenberg <ariebovenberg@wlnss.com>
2018-01-21 19:36:42 +0100

    remove unused utils

--- snug/__about__.py
+++ snug/__about__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __author__ = 'Arie Bovenberg'
 __copyright__ = '2017, Arie Bovenberg'
 __description__ = 'Wrap web APIs to fit nicely into your python code'
