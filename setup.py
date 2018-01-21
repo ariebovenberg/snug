5839828e2954c365d56b3d6a5536131c9ff6613b
Arie Bovenberg <ariebovenberg@wlnss.com>
2018-01-21 19:37:31 +0100

    make typing requirement less strict

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    install_requires=[] if sys.version_info > (3, 5) else ['typing==3.6.2'],
+    install_requires=[] if sys.version_info > (3, 5) else ['typing>=3.6.2'],
     keywords=['api', 'wrapper', 'rest', 'http'],
     python_requires='>=3.4',
     packages=find_packages(exclude=('tests', 'docs', 'examples'))
