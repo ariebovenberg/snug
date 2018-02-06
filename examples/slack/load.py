8fd5b84c0ad053e0900bcb811371dc23edfcba64
Arie Bovenberg <ariebovenberg@wlnss.com>
2018-02-06 19:46:16 +0100

    fix in slack API

--- examples/slack/load.py
+++ examples/slack/load.py
@@ -11,9 +11,13 @@ def page_loader(subloaders, value):
     loader, = subloaders
     objects_key, = (k for k in value
                     if k != 'response_metadata' and k != 'ok')
+    try:
+        next_cursor = value['response_metadata']['next_cursor']
+    except KeyError:
+        next_cursor = None
     return types.Page(
         list(map(loader, value[objects_key])),
-        next_cursor=value['response_metadata']['next_cursor']
+        next_cursor=next_cursor
     )
 
 
