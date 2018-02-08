2dd278e6d8241e751da7cb5e4f101ceb0bcbb5dd
Arie Bovenberg <ariebovenberg@wlnss.com>
2018-02-08 21:07:06 +0100

    set beta classifier

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     author_email='a.c.bovenberg@gmail.com',
 
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
 
